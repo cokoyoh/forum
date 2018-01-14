f70ad2b7115414736dd284545727993d5443bdf1
cokoyoh <charlesokoyoh@gmail.com>
2018-01-14 15:31:03 +0300

    Extracting components from blades

---
+++ resources/assets/js/components/Replies.vue
@@ -0,0 +1,34 @@
+<template>
+   <div>
+       <div v-for="(reply, index) in items">
+           <reply :data = "reply" @deleted="remove(index)"></reply>
+       </div>
+   </div>
+</template>
+<script>
+    import Reply from './Reply.vue';
+    export default {
+        props: ['data'],
+
+        data() {
+            return {
+                items: this.data,
+            };
+        },
+        components: {
+            'reply' : Reply
+        },
+
+        methods : {
+            remove(index)
+            {
+                this.items.splice(index, 1);
+
+                this.$emit('removed');
+
+                flash('Reply was deleted');
+            }
+        }
+    }
+
+</script>
--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -1,12 +1,43 @@
+<template>
+    <div class = "panel panel-default" :id="'reply-' + id">
+        <div class = "panel-heading">
+            <div class="level">
+                <h5 class="flex">
+                    <a :href = "'/profiles/'+ data.owner.name"
+                    v-text="data.owner.name"> </a>
+                    said  {{data.created_at}}
+                </h5>
+                <div v-if="signedIn">
+                    <favourites :reply="data"></favourites>
+                </div>
+            </div>
+        </div>
+        <div class = "panel-body">
+            <div v-if="editing">
+                <div class="form-group">
+                    <textarea class="form-control" v-model="body"></textarea>
+                </div>
+                <button class="btn btn-xs btn-primary" @click="update">Update</button>
+                <button class="btn btn-xs btn-link" @click="editing = false">Cancel</button>
+            </div>
+            <div v-else v-text="body"></div>
+        </div>
+        <div class="panel-footer level" v-if="canUpdate">
+            <button class="btn btn-xs mr-1" @click="editing = true" >Edit</button>
+            <button class="btn btn-xs btn-danger mr-1" @click="destroy">Delete</button>
+        </div>
+    </div>
+</template>
 <script>
     import Favourites from './Favourite.vue'
     export default {
-        props: ['attributes'],
+        props: ['data'],
 
         data() {
             return {
                 editing: false,
-                body: this.attributes.body
+                body: this.data.body,
+                id : this.data.id,
             };
         },
 
@@ -14,9 +45,20 @@
           'favourites' : Favourites
         },
 
+        computed: {
+          signedIn()
+          {
+              return window.App.signedIn;
+          },
+          canUpdate()
+          {
+             return this.authorize(user => this.data.user_id == user.id);
+          }
+        },
+
         methods: {
             update() {
-                axios.patch('/replies/' + this.attributes.id, {body: this.body});
+                axios.patch('/replies/' + this.data.id, {body: this.body});
 
                 this.editing = false;
 
@@ -27,12 +69,9 @@
 
             destroy()
             {
-                axios.delete('/replies/' + this.attributes.id);
+                axios.delete('/replies/' + this.data.id);
 
-                $(this.$el).fadeOut(300, () => {
-                    flash('Your reply has been deleted');
-                });
-
+                this.$emit('deleted', this.data.id);
             }
 
         }
