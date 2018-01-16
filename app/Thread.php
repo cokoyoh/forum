599e3d8b6f4040c5c3a240a2f341721e06204a40
cokoyoh <charlesokoyoh@gmail.com>
2018-01-16 19:57:06 +0300

    Refactor further and remove events and listeners

--- app/Thread.php
+++ app/Thread.php
@@ -49,14 +49,17 @@ class Thread extends Model
     {
         $reply =  $this->replies()->create($reply);
 
-        event(new ThreadHasNewReply($this, $reply));
-
-//        $this->subscriptions
-//            ->where('user_id', '!=', $reply->user_id)
-//            ->each
-//            ->notify($reply);
+        $this->notifySubscribers($reply);
 
          return $reply;
+    }
+
+    public function notifySubscribers($reply)
+    {
+        $this->subscriptions
+            ->where('user_id', '!=', $reply->user_id)
+            ->each
+            ->notify($reply);
     }
 
     public function scopeFilter($query, $filters)
